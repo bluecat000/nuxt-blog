22c296b3b832b67c56a3b34d7c3f6072c267d189
bluecat000@outlook.com <bluecat@localhost.bluecat>
2017-08-17 11:09:41 -0400

    edit style of admin/edit

--- pages/admin/edit.vue
+++ pages/admin/edit.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="content">
-      <section class="left-sec">
-        <form role="form"> 
-          <div class="form-group"> 
-            <!-- <textarea id="articel" class="form-control" :value="text" @input="text = $event.target.value"></textarea>  -->
-            <textarea id="articel" class="form-control" :value="text" @input="updateText"></textarea> 
-            <!-- @input="updateText()" 不能引用$event 要手动传参 -->
-          </div> 
-        </form>  
-      </section>
-      <section class="right-sec" v-html="reflashView"></section>
+    <div class="sub-til">
+      <span class="sub-til-text">NEW BOLG</span>
+      <div class="btn-group">
+        <button type="button" class="btn btn-primary">Action</button>
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span class="caret"></span>
+          <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href="#">save</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">save in draft</a></li>
+        </ul>
+      </div>
+    </div>
+    <section class="left-sec">
+      <form role="form"> 
+        <div class="form-group"> 
+          <!-- <textarea id="articel" class="form-control" :value="text" @input="text = $event.target.value"></textarea>  -->
+          <textarea id="articel" class="form-control" :value="text" @input="updateText"></textarea> 
+          <!-- @input="updateText()" 如果引用$event 要手动传参 -->
+        </div> 
+      </form>  
+    </section>
+    <section class="right-sec" v-html="reflashView"></section>
   </div>
 </template>
 <script>
@@ -36,7 +51,15 @@ export default {
 <style lang="scss" scope>
 .content{
   width: 100%;
+  .sub-til{
+    .sub-til-text{
+      font-size: 3rem;
+      vertical-align: middle;
+      margin-right: 2rem;
+    }
+  }
   .left-sec{
+    padding-top: 2rem;
     float: left;
     form{
       width:100%;
@@ -44,10 +67,10 @@ export default {
         min-height: 60rem;
       }
     }
-    width: 40%;
+    width: 48%;
   }
   .right-sec{
-    width: 40%;
+    width: 48%;
     float: right;
   }
 }
